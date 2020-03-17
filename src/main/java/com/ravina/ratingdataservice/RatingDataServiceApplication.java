580070305b9515cd516c5d066b2aa1d6bbf2b4fe
ravina.thangavel <ravina.thangavel@kgisl.com>
2020-03-17 09:06:20 +0530

    commit changes

--- src/main/java/com/ravina/ratingdataservice/RatingDataServiceApplication.java
+++ src/main/java/com/ravina/ratingdataservice/RatingDataServiceApplication.java
@@ -2,8 +2,11 @@ package com.ravina.ratingdataservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RatingDataServiceApplication {
 
 	public static void main(String[] args) {
